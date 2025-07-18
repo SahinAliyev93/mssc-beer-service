183f4c020708ca3f056d4a78d098b0d492774a4d
ShahinAliyev <sahin.aliyev979@gmail.com>
2025-07-18 14:46:52 +0200

    Remove tracked target/classes files and apply .gitignore

---
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,70 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-07-18T14:31:36+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.4 (Eclipse Adoptium)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public BeerDto beertoBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto.BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.id( beer.getId() );
+        if ( beer.getVersion() != null ) {
+            beerDto.version( beer.getVersion().intValue() );
+        }
+        beerDto.createdDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
+        beerDto.lastModifiedDate( dateMapper.asOffsetDateTime( beer.getLastModifiedDate() ) );
+        beerDto.beerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.beerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
+        }
+        beerDto.upc( beer.getUpc() );
+        beerDto.price( beer.getPrice() );
+        beerDto.quantityOnHand( beer.getQuantityOnHand() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        if ( beerDto == null ) {
+            return null;
+        }
+
+        Beer.BeerBuilder beer = Beer.builder();
+
+        beer.id( beerDto.getId() );
+        if ( beerDto.getVersion() != null ) {
+            beer.version( beerDto.getVersion().longValue() );
+        }
+        beer.createdDate( dateMapper.asTimestamp( beerDto.getCreatedDate() ) );
+        beer.lastModifiedDate( dateMapper.asTimestamp( beerDto.getLastModifiedDate() ) );
+        beer.beerName( beerDto.getBeerName() );
+        if ( beerDto.getBeerStyle() != null ) {
+            beer.beerStyle( beerDto.getBeerStyle().name() );
+        }
+        beer.upc( beerDto.getUpc() );
+        beer.price( beerDto.getPrice() );
+        beer.quantityOnHand( beerDto.getQuantityOnHand() );
+
+        return beer.build();
+    }
+}
